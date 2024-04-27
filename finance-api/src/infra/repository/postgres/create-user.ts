a5859e2da53b3fc6bb0ad519c8e3aa2a61be6e79
rafaelluiis2315 <rafael.luisbarreto23@gmail.com>
2024-04-27 20:41:21 -0300

    fix(api): ensure created user is returned on create user repository

--- finance-api/src/infra/repository/postgres/create-user.ts
+++ finance-api/src/infra/repository/postgres/create-user.ts
@@ -12,9 +12,10 @@ interface CreateUserParams {
 @Injectable()
 export class PostgresCreateUserRepository {
   constructor(@Inject(Postgres) private readonly postgresClient: Postgres) {}
+
   async execute(createUserParams: CreateUserParams) {
-    const results = await this.postgresClient.exec({
-      query: `INSERT INTO public.users (ID, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5);`,
+    await this.postgresClient.exec({
+      query: `INSERT INTO public.users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5);`,
       params: [
         createUserParams.id,
         createUserParams.firstName,
@@ -24,6 +25,11 @@ export class PostgresCreateUserRepository {
       ],
     });
 
-    return results;
+    const userCreated = await this.postgresClient.exec({
+      query: `SELECT * FROM public.users WHERE id = $1;`,
+      params: [createUserParams.id],
+    });
+
+    return userCreated[0];
   }
 }
