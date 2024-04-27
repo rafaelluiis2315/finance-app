946f1cfbe58487e23a058c4fc316346bc5a9f10e
rafaelluiis2315 <rafael.luisbarreto23@gmail.com>
2024-04-27 20:46:43 -0300

    refactor(api): remove unnecessary filed

--- finance-api/src/app.module.ts
+++ finance-api/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { InfraModule } from './infra/infra.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), InfraModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
