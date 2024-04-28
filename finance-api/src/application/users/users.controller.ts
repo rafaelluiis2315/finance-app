38807ca977dfb9743b9fd45ac0edbd233c207c41
rafaelluiis2315 <rafael.luisbarreto23@gmail.com>
2024-04-27 22:54:32 -0300

    feat(api): add getById in users router

--- finance-api/src/application/users/users.controller.ts
+++ finance-api/src/application/users/users.controller.ts
@@ -3,13 +3,21 @@ import {
   Post,
   Body,
   InternalServerErrorException,
+  Get,
+  Param,
+  BadRequestException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { CreateUserUseCase } from '../use-cases/user/create-user';
+import { GetUserByIdUseCase } from '../use-cases/user/get-user- by-id';
+import { isUUID } from 'class-validator';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly createUser: CreateUserUseCase) {}
+  constructor(
+    private readonly createUser: CreateUserUseCase,
+    private readonly getUserById: GetUserByIdUseCase,
+  ) {}
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
@@ -19,5 +27,14 @@ export class UserController {
       console.error(error);
       throw new InternalServerErrorException('Internal server error');
     }
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    const isIdValid = isUUID(id);
+    if (!isIdValid) {
+      throw new BadRequestException('The provided id is not valid.');
+    }
+    return await this.getUserById.execute(id);
   }
 }
