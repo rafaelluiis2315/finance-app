39413e1a05455a1d001e6a0574228ecc6850562b
rafaelluiis2315 <rafael.barreto@neri.care>
2025-06-14 22:05:05 -0300

    feat(api): enhance UpdateTransactionDto and implement update endpoint in TransactionsController

--- finance-api/src/application/transactions/dto/update-transaction.dto.ts
+++ finance-api/src/application/transactions/dto/update-transaction.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { CreateTransactionDto } from './create-transaction.dto';
 
-export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {}
+export class UpdateTransactionDto extends OmitType(
+  PartialType(CreateTransactionDto),
+  ['userId'],
+) {}
--- finance-api/src/application/transactions/transactions.controller.ts
+++ finance-api/src/application/transactions/transactions.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 
 import { isCurrency } from 'class-validator';
@@ -7,6 +17,8 @@ import { InvalidIdError } from '../errors/user.exception';
 import { checkIdIsValid } from '../helpers/user';
 import { CreateTransactionUseCase } from '../use-cases/transaction/create-transaction';
 import { GetTransactionsByUserIdUseCase } from '../use-cases/transaction/get-transactions-by-user-id';
+import { UpdateTransactionUseCase } from '../use-cases/transaction/update-transaction';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
 
 @Controller('transactions')
 export class TransactionsController {
@@ -15,6 +27,8 @@ export class TransactionsController {
     private readonly createTransactionUseCase: CreateTransactionUseCase,
     @Inject(GetTransactionsByUserIdUseCase)
     private readonly getTransactionsByUserIdUseCase: GetTransactionsByUserIdUseCase,
+    @Inject(UpdateTransactionUseCase)
+    private readonly updateTransactionUseCase: UpdateTransactionUseCase,
   ) {}
 
   @Post()
@@ -46,5 +60,26 @@ export class TransactionsController {
     }
 
     return this.getTransactionsByUserIdUseCase.execute(userId);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateTransaction: UpdateTransactionDto,
+  ) {
+    if (!checkIdIsValid(id)) {
+      throw new InvalidIdError();
+    }
+
+    if (!updateTransaction || Object.keys(updateTransaction).length === 0) {
+      throw new HttpException('Update data is required', 400);
+    }
+
+    const transaction = await this.updateTransactionUseCase.execute({
+      id,
+      ...updateTransaction,
+    });
+
+    return transaction;
   }
 }
